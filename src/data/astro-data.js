026459de9aa247f53713b96a76e953b6d4537264
ankitjnvynr <ankitbkana@outlook.com>
2024-12-26 17:03:19 +0530

    first commit

--- src/data/astro-data.js
+++ src/data/astro-data.js
@@ -1,15 +1,11 @@
 export const astroData = {
-  para: `Book An Astrologer Schedule a Phone Call or WhatsApp Consultation with
-          Pandit/Guruji. Explore the marathisarvapooja Astrology Service – Your
-          Pathway to Booking Online Phone Consultations. Panditji/guruji has 15+
-          years of experience in puja homa and Havan and 12+ years of experience
-          in Astrology. Experience unparalleled Online Astrology Services with
-          marathisarvapooja. Choose Your Preferred Language and Suitable Time
-          Slot for Consultation – Options Include Hindi, Kannada, Marathi, and
-          More. Secure Your Direct Appointment with Pandit/Guruji Online – No
-          Middle-Person Involved. Pandit/Guruji Excel in Providing Predictive
-          Insights for Life, Love, Career, Education, Health, and More. These
-          Insights Are Crafted from the Birthdate You Provide During Booking.`,
+  para: `Book an Astrologer and schedule a Phone Call or WhatsApp consultation with a highly knowledgeable and experienced Pandit or Guruji. Discover the Vidwat of authentic Astrology Services – your gateway to online consultations with seasoned professionals.
+
+With over 15+ years of expertise in performing Pujas, Homas, and Havans, and 12+ years of experience in Astrology, our Pandit/Guruji bring unmatched wisdom and precision to their services. Experience the true depth of Vidwat, where traditional knowledge meets modern convenience.
+
+Choose your preferred language – Hindi, Kannada, Marathi, or others – and select a suitable time slot for your personalized consultation. Secure a direct appointment with our Pandit/Guruji online, with no intermediaries involved, ensuring authenticity and privacy.
+
+Our experts specialize in providing predictive insights on life, love, career, education, health, and more. These predictions are thoughtfully derived from the birth details you share during the booking process. Step into the world of unparalleled astrology services, where tradition, wisdom, and accuracy converge to illuminate your path forward.`,
   heading: [
     "Consult Our Expert Vedic Astrologer",
     `Experience Top-notch Vedic Astrology Excellence. Get in Touch with Panditji/Guruji`,
